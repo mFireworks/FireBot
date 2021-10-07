c062f0b9138d7da9f19cffeee766ac49a7e1a112
Michael Long <longmichael97@gmail.com>
2021-10-06 21:52:15 -0700

    Fleshed out beginning of bot
    Next is the actual connection to roles

---
+++ src/Assignbot.py
@@ -0,0 +1,41 @@
+# Using discord.py for Discord API - https://discordpy.readthedocs.io/en/stable/index.html
+import discord
+from discord.ext import commands
+# Using APSW for SQLite Database Management - https://rogerbinns.github.io/apsw/example.html
+import apsw
+# Using json for config stuff
+import json
+
+# Initialize Bot
+#intents = discord.Intents(guilds=True, members=True, reactions=True)
+client = commands.Bot(command_prefix='.')
+
+# Load Configuration Options
+configFile = open('data/config.json')
+config = json.load(configFile)
+configFile.close()
+
+# Establish Connection to Database
+database = apsw.Connection("data/db.sqlite")
+dbCursor = database.cursor()
+
+# Events
+@client.event
+async def on_ready():
+    print('AssignBot Turned On...')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def shutdown(ctx):
+    await ctx.send("Bot is shutting down.")
+    await client.close()
+    print("AssignBot Closed...")
+
+#@client.command()
+#async def setjoinrole(ctx, joinRoleID):
+
+
+client.run(config['token'])
