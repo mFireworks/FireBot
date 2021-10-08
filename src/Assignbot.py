d5c473a9a7f95f24585e20f43e9e8b143573d982
Michael Long <longmichael97@gmail.com>
2021-10-07 20:47:13 -0700

    Setup Auto-Joining Feature
    Woohoo!

--- src/Assignbot.py
+++ src/Assignbot.py
@@ -7,8 +7,8 @@ import apsw
 import json
 
 # Initialize Bot
-#intents = discord.Intents(guilds=True, members=True, reactions=True)
-client = commands.Bot(command_prefix='.')
+intents = discord.Intents(guilds=True, members=True, messages=True, reactions=True)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 # Load Configuration Options
 configFile = open('data/config.json')
@@ -20,23 +20,49 @@ database = apsw.Connection("data/db.sqlite")
 dbCursor = database.cursor()
 
 # Events
-@client.event
+@bot.event
 async def on_ready():
     print('AssignBot Turned On...')
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+@bot.event
+async def on_member_join(member):
+    print("We have triggered the join event")
+    joinRole = list(dbCursor.execute("select JoinRoleID from main where ServerID = ?", (member.guild.id, )))
+    if (len(joinRole) == None or len(joinRole) > 1):
+        print(member.guild.name + " doesn't have a join role set")
+        return
 
-@client.command()
+    role = discord.utils.get(member.guild.roles, id=int(joinRole[0][0]))
+    await member.add_roles(role)
+    print("Sent " + member.name + " to auto-join role " + role.name)
+
+# Commands
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command()
 async def shutdown(ctx):
+    if (ctx.author.id != int(config['ownerID'])):
+        return
     await ctx.send("Bot is shutting down.")
-    await client.close()
+    await bot.close()
     database.close(True)
     print("AssignBot Closed...")
 
-#@client.command()
-#async def setjoinrole(ctx, joinRoleID):
+@bot.command()
+async def setjoinrole(ctx, joinRoleID):
+    joinRole = ctx.guild.get_role(int(joinRoleID))
+    if (joinRole == None):
+        await ctx.send("Provided role not found.")
+        return
+
+    serverEntries = list(dbCursor.execute("select ServerID from main where ServerID = ?", (ctx.guild.id, )))
+    if (len(serverEntries) == 0):
+        dbCursor.execute("insert into main (ServerID, JoinRoleID) values (?, ?)", (ctx.guild.id, joinRoleID))
+    else:
+        dbCursor.execute("update main set JoinRoleID = ? where ServerID = ?", (joinRoleID, ctx.guild.id))
+    await ctx.send(joinRole.name + " has been set as the auto-join role for " + ctx.guild.name)
 
 
-client.run(config['token'])
+bot.run(config['token'])
