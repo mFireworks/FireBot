257cbf2994b006e320681d2ad8034c8d0c2e12d0
Michael Long <longmichael97@gmail.com>
2021-10-07 21:13:43 -0700

    Setup Logging Channel Feature
    Didn't fully test, but it seems to behave as expected

--- src/Assignbot.py
+++ src/Assignbot.py
@@ -19,6 +19,24 @@ configFile.close()
 database = apsw.Connection("data/db.sqlite")
 dbCursor = database.cursor()
 
+# Helper Functions
+async def logMessage(guild, defaultChannel, message):
+    loggingID = list(dbCursor.execute("select LoggingChannelID from main where ServerID = ?", (guild.id, )))
+    if (len(loggingID) == 0):
+        if (defaultChannel == None):
+            print(message)
+        else:
+            await defaultChannel.send(message)
+        return
+    logChannel = guild.get_channel(int(loggingID[0][0]))
+    if (logChannel == None):
+        if (defaultChannel == None):
+            print(message)
+        else:
+            await defaultChannel.send(message)
+        return
+    await logChannel.send(message)
+
 # Events
 @bot.event
 async def on_ready():
@@ -27,14 +45,14 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print("We have triggered the join event")
-    joinRole = list(dbCursor.execute("select JoinRoleID from main where ServerID = ?", (member.guild.id, )))
-    if (len(joinRole) == None or len(joinRole) > 1):
-        print(member.guild.name + " doesn't have a join role set")
+    joinRoleID = list(dbCursor.execute("select JoinRoleID from main where ServerID = ?", (member.guild.id, )))
+    if (len(joinRoleID) == 0):
+        await logMessage(member.guild, None, member.guild.name + " doesn't have a join role set")
         return
 
-    role = discord.utils.get(member.guild.roles, id=int(joinRole[0][0]))
+    role = discord.utils.get(member.guild.roles, id=int(joinRoleID[0][0]))
     await member.add_roles(role)
-    print("Sent " + member.name + " to auto-join role " + role.name)
+    await logMessage(member.guild, None, "Sent " + member.name + " to auto-join role " + role.name)
 
 # Commands
 @bot.command()
@@ -45,7 +63,7 @@ async def ping(ctx):
 async def shutdown(ctx):
     if (ctx.author.id != int(config['ownerID'])):
         return
-    await ctx.send("Bot is shutting down.")
+    await logMessage(ctx.guild, ctx.channel, "Bot is shutting down.")
     await bot.close()
     database.close(True)
     print("AssignBot Closed...")
@@ -54,7 +72,7 @@ async def shutdown(ctx):
 async def setjoinrole(ctx, joinRoleID):
     joinRole = ctx.guild.get_role(int(joinRoleID))
     if (joinRole == None):
-        await ctx.send("Provided role not found.")
+        await logMessage(ctx.guild, ctx.channel, "Provided role not found.")
         return
 
     serverEntries = list(dbCursor.execute("select ServerID from main where ServerID = ?", (ctx.guild.id, )))
@@ -62,7 +80,15 @@ async def setjoinrole(ctx, joinRoleID):
         dbCursor.execute("insert into main (ServerID, JoinRoleID) values (?, ?)", (ctx.guild.id, joinRoleID))
     else:
         dbCursor.execute("update main set JoinRoleID = ? where ServerID = ?", (joinRoleID, ctx.guild.id))
-    await ctx.send(joinRole.name + " has been set as the auto-join role for " + ctx.guild.name)
+    await logMessage(ctx.guild, ctx.channel, joinRole.name + " has been set as the auto-join role for " + ctx.guild.name)
 
+@bot.command()
+async def setloggingchannel(ctx):
+    serverEntry = list(dbCursor.execute("select ServerID from main where ServerID = ?", (ctx.guild.id, )))
+    if (len(serverEntry) == 0):
+        dbCursor.execute("insert into main (ServerID, LoggingChannelID) values (?, ?)", (ctx.guild.id, ctx.channel.id))
+    else:
+        dbCursor.execute("update main set LoggingChannelID = ? where ServerID = ?", (ctx.channel.id, ctx.guild.id))
+    await logMessage(ctx.guild, ctx.channel, "#" + ctx.channel.name + " set as the logging channel for AssignBot.")
 
 bot.run(config['token'])
